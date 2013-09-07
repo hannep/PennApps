af777f11e4a2e4448390484ba1e9f2a6b027b4bb
paine.hanne@gmail.com <paine.hanne@gmail.com>
2013-09-07 04:04:42 -0400

    Here's a hideous table.

--- app.py
+++ app.py
@@ -6,9 +6,11 @@ import xml.etree.ElementTree as ET
 import sqlite3
 import os
 import datetime
-
+from jinja2 import FileSystemLoader 
+from jinja2 import Environment
 path = os.path.abspath(os.path.dirname(__file__))
-
+loader = FileSystemLoader(path) 
+env = Environment(loader=loader)
 
 PUNCTUATION = "!@#$%^&*()+{}[]`~\\|'\";:/?.>,<"
 
@@ -16,13 +18,20 @@ class Root(object):
 
     def getConnection(self): 
         return sqlite3.connect(os.path.join(path, "answers.db"))  
+    
+    def parse_row(self, row): 
+        return {"number": row[0], "answer": row[1], "correct": row[2], "timestamp": row[3]}
+        
 
     def dashboard(self) : 
+        template = env.get_template('template.html')
         with self.getConnection() as conn: 
             cursor = conn.cursor() 
             cursor.execute("SELECT * FROM Texts;")
             rows = cursor.fetchall()
-        return str(rows)
+            texts = [self.parse_row(row) for row in rows]
+        return template.render(texts=texts) 
+        #return str(rows)
         
     
     dashboard.exposed = True  
