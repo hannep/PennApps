36b58e03c438edca371b08c18b887ba088b9c8e5
Mark Fielbig <mfielbig@gmail.com>
2013-09-07 23:37:38 -0400

    integrated login with backend.

--- app/main.py
+++ app/main.py
@@ -9,37 +9,6 @@ import webapp2
 from utils import renderer
 from game import *
 
-###
-# Temporary constants for interface dev
-###
-MOCK_GAMES = [{
-	'id': 0,
-	'name': 'Super awesome game',
-	'description': 'This game is so awesome omg omg.',
-	'participants': [
-		{ 'name': 'Mark Fielbig' },
-		{ 'name': 'Phil Huff' }
-	],
-	'puzzles': [{ 
-		'name': 'mindfuck' ,
-		'description': 'omgwtfsolvethisshit',
-		'answer': '42'
-	}]
-}, {
-	'id': 1,
-	'name': 'Not so awesome game',
-	'description': 'Weeeeeeeeak.',
-	'participants': [
-		{ 'name': 'Mark Fielbig' },
-		{ 'name': 'Phil Huff' }
-	],
-	'puzzles': [{ 
-		'name': 'lolwhat?' ,
-		'description': 'easssssy',
-		'answer': ':)'
-	}]
-}]
-
 game = Game(name='Game', description='Default game')
 
 ###
@@ -92,6 +61,20 @@ class AdminPuzzleEditController(webapp2.RequestHandler):
 			puzzle.answer = self.request.get('answer')
 			puzzle.question = self.request.get('question')
 		self.get()
+
+class RegisterTeamController(webapp2.RequestHandler):
+	def get(self):
+		rendered = renderer.render('client/registerteam.html', {})
+		self.response.write(rendered)
+
+	def post(self):
+		country_code = self.request.get('countrycode')
+		mobile_number = self.request.get('cellNum')
+		phone_number = country_code + mobile_number
+		name = self.request.get('teamName')
+		user = User(phone_number, name)
+		game.users.append(user)
+		self.response.write(str(user))
 	
 class PuzzleAnswerController(webapp2.RequestHandler):
 	def post(self):
@@ -116,7 +99,6 @@ class PuzzleAnswerController(webapp2.RequestHandler):
 		else:
 			return
 		answer = Answer(user, minigame, data)
-		#get singleton Game object
 		
 ###
 # Routes
@@ -125,5 +107,5 @@ application = webapp2.WSGIApplication([
 	('/admin', AdminGameController),
 	('/admin/edit', AdminGameEditController),
 	('/admin/puzzle/edit', AdminPuzzleEditController),
-    ('/', IndexController)
+    ('/', RegisterTeamController)
 ], debug=True)
