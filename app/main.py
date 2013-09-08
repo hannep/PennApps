d91b7963ef32d41c448f6b56a0402289f04c2bbe
phroph <phroph@yahoo.com>
2013-09-08 02:20:13 -0400

    Puzzles 'n stuff

--- app/main.py
+++ app/main.py
@@ -100,6 +100,12 @@ class HelpController(webapp2.RequestHandler):
 		})
 		self.get()
 	
+class PuzzleSelectionController(webapp2.RequestHandler):
+	def get(self):
+		rendered = renderer.render('client/playergamehome.html', {})
+		self.response.write(rendered)
+	def post(self):
+		self.redirect('/challenge')
 class PuzzleAnswerController(webapp2.RequestHandler):
 	def get(self):
 		rendered = renderer.render('client/challenge.html', {})
@@ -141,6 +147,7 @@ application = webapp2.WSGIApplication([
 	('/admin/puzzle/edit', AdminPuzzleEditController),
 	('/admin/notify', AdminNotificationController),
 	('/challenge', PuzzleAnswerController),
+	('/puzzles', PuzzleSelectionController),
 	('/help', HelpController),
     ('/', RegisterTeamController)
 ], debug=True)
